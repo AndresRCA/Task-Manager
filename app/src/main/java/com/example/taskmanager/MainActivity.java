b1098071f8ac45b2f49459b0fcb39344c18bd682
Andrew <andrescastillo.r@hotmail.com>
2019-07-05 01:05:55 -0400

    set up main activity for TaskAdapter

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.taskmanager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -12,22 +15,51 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private final static int ADD_TASK = 1;
+    private Task[] task_data;
+    private RecyclerView recyclerView;
+    private TaskAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        /*
+        * Llena task_data aqui usando la base de datos
+        * */
+        task_data = new Task[]{new Task(1, "Tarea 1", "", "2019::29/05::12:00 PM", true), new Task(2, "Tarea 2", "", "2019::12/05::17:00 PM", false)};
+
+        // specify an adapter (see also next example)
+        mAdapter = new TaskAdapter(task_data);
+        recyclerView.setAdapter(mAdapter);
+
+        // toolbar and fab
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), AddTask.class);
+                Intent intent = new Intent(getBaseContext(), AddTask.class);
                 startActivityForResult(intent, ADD_TASK);
             }
         });
+    }
+
+    public void onActivityResult(int request_code, int result_code, Intent data) {
+        super.onActivityResult(request_code, result_code, data);
+        // actualiza base de datos
     }
 
     @Override
