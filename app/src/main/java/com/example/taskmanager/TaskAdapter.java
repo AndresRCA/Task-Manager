c54d7838af040f630e77cdbf3d795a7b9c0de2ec
Andrew <andrescastillo.r@hotmail.com>
2019-07-05 21:33:39 -0400

    task adapter to use in main and task_item.xml

--- app/src/main/java/com/example/taskmanager/TaskAdapter.java
+++ app/src/main/java/com/example/taskmanager/TaskAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> {
@@ -11,10 +12,16 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> 
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView textView;
+        public TextView description;
+        public CheckBox is_completed;
+        public TextView date;
+        public TextView time;
         public MyViewHolder(View v) {
             super(v);
-            textView = v.findViewById(R.id.textView);
+            description = v.findViewById(R.id.description);
+            is_completed = v.findViewById(R.id.is_completed);
+            date = v.findViewById(R.id.date);
+            time = v.findViewById(R.id.time);
         }
     }
 
@@ -32,7 +39,10 @@ public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.MyViewHolder> 
 
     @Override
     public void onBindViewHolder(TaskAdapter.MyViewHolder holder, int i) {
-        holder.textView.setText(task_data[i].getDescription());
+        holder.description.setText(task_data[i].getDescription());
+        holder.is_completed.setChecked(task_data[i].getIs_completed());
+        holder.date.setText(task_data[i].getComplete_time()); // for now I wont separate time and date
+        holder.time.setText(task_data[i].getComplete_time());
     }
 
     @Override
