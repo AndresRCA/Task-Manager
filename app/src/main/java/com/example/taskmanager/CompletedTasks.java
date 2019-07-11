e5f5c211abe250a4ba9b2683674c48c42a1776c0
Andrew <andrescastillo.r@hotmail.com>
2019-07-11 08:32:27 -0400

    add RecyclerView

--- app/src/main/java/com/example/taskmanager/CompletedTasks.java
+++ app/src/main/java/com/example/taskmanager/CompletedTasks.java
@@ -15,7 +15,7 @@ public class CompletedTasks extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_completed_tasks);
 
-        recyclerView = findViewById(R.id.my_recycler_view);
+        recyclerView = findViewById(R.id.my_recycler_view2);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
